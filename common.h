2deb4f33022569b6ad0b791a5c57cfe97dca8cd3
asd. <youwilldie6.066@gmail.com>
2024-09-06 23:25:55 +0300

    Store libraries here

---
+++ common.h
@@ -0,0 +1,15 @@
+// Import libraries to use them in main.cpp
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <unordered_set>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <cctype>
+
+#endif  // COMMON_H
